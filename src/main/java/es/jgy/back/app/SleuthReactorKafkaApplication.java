ffc77383556586a5e1e242ea9ce99a9d9be66707
Jorge Guerra Yerpes <jorgegueyer@gmail.com>
2021-06-01 09:18:46 +0200

    First commit
    Adding files project

---
+++ src/main/java/es/jgy/back/app/SleuthReactorKafkaApplication.java
@@ -0,0 +1,13 @@
+package es.jgy.back.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthReactorKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthReactorKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/jgy/back/app/SleuthReactorKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jgy.back.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthReactorKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
